71380c3337a7aca840e1df2994540371f6cb81d3
Mat Jordan <mat@northwestern.edu>
2021-11-30 22:51:05 -0500

    Rip node.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,150 @@
+const fetch = require("node-fetch")
+const axios = require("axios")
+const slugify = require("slugify")
 const path = require(`path`)
+const merge = require("lodash/merge")
 const { createFilePath } = require(`gatsby-source-filesystem`)
+
+/*
+ * Set root IIIF Collection conforming to specification
+ * at https://iiif.io/api/presentation/3.0/#51-collection
+ */
+const rootCollection =
+  "https://digital.lib.utk.edu/static/iiif/collections/rfta_completed.json"
+
+exports.sourceNodes = async ({
+  actions,
+  createNodeId,
+  createContentDigest,
+  graphql,
+}) => {
+  const { createNode } = actions
+
+  const response = await fetch(rootCollection)
+  const json = await response.json()
+  const { items = [] } = json
+
+  const manifests = await chunks(
+    items,
+    async manifest => {
+      const { id } = manifest
+      return axios.get(id).then(result => result.data)
+    },
+    25
+  )
+
+  let metadataNodes = {}
+
+  manifests.forEach((node, index) => {
+    node.manifestId = node.id
+
+    node.slug = `object/${slugify(
+      node.label.en[0].replace("Interview with ", ""),
+      {
+        replacement: "-",
+        lower: true,
+        strict: true,
+        trim: true,
+      }
+    )}`
+
+    node.transcripts = ((items, transcripts = []) => {
+      if (Array.isArray(items)) {
+        items[0].items[0].items.map(function (element) {
+          if (
+            element.motivation === "supplementing" &&
+            element.body[0].format === "text/vtt"
+          ) {
+            transcripts.push(element.body[0])
+          }
+        })
+      }
+      return transcripts
+    })(node.items)
+
+    let nodeId = createNodeId(`Manifests-${node.id}`)
+
+    createNode({
+      ...node,
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: "Manifests",
+        content: JSON.stringify(node),
+        contentDigest: createContentDigest(node),
+      },
+    })
+
+    let metadata = {}
+
+    for (const element of node.metadata) {
+      let slug = `browse/${slugify(element.label.en[0], {
+        replacement: "-",
+        lower: true,
+        strict: true,
+        trim: true,
+      })}`
+
+      metadata[slug] = {}
+      metadata[slug].slug = slug
+      metadata[slug].label = element.label.en[0]
+
+      metadata[slug].values = {}
+
+      for (const string of element.value.en) {
+        let valueSlug = slugify(string, {
+          replacement: "_",
+          lower: true,
+          strict: true,
+          trim: true,
+        })
+        metadata[slug].values[valueSlug] = {}
+        metadata[slug].values[valueSlug].slug = valueSlug
+        metadata[slug].values[valueSlug].label = string
+        metadata[slug].values[valueSlug].manifests = {}
+        metadata[slug].values[valueSlug].manifests[nodeId] = {
+          id: nodeId,
+          slug: node.slug,
+          manifestId: node.manifestId,
+          label: node.label,
+          summary: node.summary,
+          thumbnail: node.thumbnail,
+        }
+      }
+    }
+
+    merge(metadataNodes, metadata)
+  })
+
+  for (const property in metadataNodes) {
+    const metadata = metadataNodes[property]
+    createNode({
+      slug: metadata.slug,
+      label: metadata.label,
+      values: tidyObjectArrays(metadata.values),
+      id: createNodeId(`Metadata-${metadata.slug}`),
+      parent: null,
+      children: [],
+      internal: {
+        type: "Metadata",
+        content: JSON.stringify(metadata),
+        contentDigest: createContentDigest(metadata),
+      },
+    })
+  }
+}
+
+tidyObjectArrays = values => {
+  return Object.keys(values).map(valueSlug => {
+    values[valueSlug].manifests = Object.keys(values[valueSlug].manifests).map(
+      nodeId => {
+        return values[valueSlug].manifests[nodeId]
+      }
+    )
+    return values[valueSlug]
+  })
+}
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
@@ -112,4 +257,40 @@ exports.createSchemaCustomization = ({ actions }) => {
       slug: String
     }
   `)
+}
+
+/*
+ * Embedded axios based request handlers
+ */
+function all(items, fn) {
+  const promises = items.map(item => fn(item))
+  return Promise.all(promises)
+}
+
+function series(items, fn) {
+  let result = []
+  return items
+    .reduce((acc, item) => {
+      acc = acc.then(() => {
+        return fn(item).then(res => result.push(res))
+      })
+      return acc
+    }, Promise.resolve())
+    .then(() => result)
+}
+
+function splitToChunks(items, chunkSize = 50) {
+  const result = []
+  for (let i = 0; i < items.length; i += chunkSize) {
+    result.push(items.slice(i, i + chunkSize))
+  }
+  return result
+}
+
+function chunks(items, fn, chunkSize = 50) {
+  let result = []
+  const chunks = splitToChunks(items, chunkSize)
+  return series(chunks, chunk => {
+    return all(chunk, fn).then(res => (result = result.concat(res)))
+  }).then(() => result)
 }
