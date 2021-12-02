ec69eb439cc703c4f38aaaf9318bca8e7a84610e
Mat Jordan <mat@northwestern.edu>
2021-12-01 22:11:19 -0500

    Cleanup layout.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `What is Can?`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
+      name: `Mat`,
     },
   },
   plugins: [
