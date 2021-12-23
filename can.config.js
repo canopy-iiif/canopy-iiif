2e86f7348583f36bd55d0e63293d931f5b850132
Mat Jordan <mat@northwestern.edu>
2021-12-23 15:41:28 -0500

    Fix temporary presentation 2.0 Hero issue.

--- can.config.js
+++ can.config.js
@@ -2,12 +2,10 @@
  * Configuration built by `npm run build`
  */
 const config = {
-  collection:
-    "https://raw.githubusercontent.com/mathewjordan/can/main/public/iiif/collection/nez-perce.json",
+  collection: "https://can-the.vercel.app/iiif/collection/nez-perce.json",
   title: "Nimíipuu, the Nez Percé",
   hero: [
-    "https://raw.githubusercontent.com/mathewjordan/can/main/public/iiif/manifest/nez-perce/08-map-territory.json",
-    "https://raw.githubusercontent.com/mathewjordan/can/main/public/iiif/manifest/nez-perce/09-joseph-dead-feast-lodge.json",
+    "https://can-the.vercel.app/iiif/manifest/nez-perce/11-watching-for-the-signal.json",
   ],
   metadata: ["Date", "Subject"],
 };
