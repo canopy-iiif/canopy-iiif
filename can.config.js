0a68ae04f5814a8c6cab3487a7dba4fd34e6bd7b
Mat Jordan <mat@northwestern.edu>
2021-12-23 08:16:55 -0500

    Extend config env vars to be more customizable.

---
+++ can.config.js
@@ -0,0 +1,7 @@
+const config = {
+  rootCollection:
+    "https://raw.githubusercontent.com/mathewjordan/can/main/public/iiif/collection/nez-perce.json",
+  siteTitle: "Nimíipuu, the Nez Percé",
+};
+
+exports.config = config;
--- next.config.js
+++ next.config.js
@@ -3,20 +3,22 @@ const {
   PHASE_PRODUCTION_BUILD,
 } = require("next/constants");
 
+const can = require("./can.config");
+
 module.exports = (phase) => {
   const isDev = phase === PHASE_DEVELOPMENT_SERVER;
   const isProd = phase === PHASE_PRODUCTION_BUILD;
 
+  const { rootCollection, siteTitle } = can.config;
+
   const env = {
     rootCollection: (() => {
       if (isDev) return "http://localhost:5001/iiif/collection/dev.json";
-      if (isProd) {
-        return "https://raw.githubusercontent.com/mathewjordan/can/main/public/iiif/collection/nez-perce.json";
-      }
+      if (isProd) return rootCollection;
     })(),
     siteTitle: (() => {
       if (isDev) return "Nimíipuu, or the Nez Percé";
-      if (isProd) return "Nimíipuu, the Nez Percé";
+      if (isProd) return siteTitle;
     })(),
   };
 
