e9635301dfceb39364a11660b8b23e53ea9c4b26
Mat Jordan <mat@northwestern.edu>
2021-12-23 15:21:06 -0500

    Adjust hero height.

--- components/Hero/Hero.styled.ts
+++ components/Hero/Hero.styled.ts
@@ -4,13 +4,13 @@ const Figure = styled("figure", {
   padding: "0",
   margin: "0",
   width: "100%",
-  height: "400px",
   overflow: "hidden",
 
   img: {
     objectFit: "cover",
     objectPosition: "center",
     width: "100%",
+    height: "400px",
   },
 });
 
