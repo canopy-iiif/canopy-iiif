8fd80c1f7108a09fe1f4403e1ed98981ed8ec20e
Mat Jordan <mat@northwestern.edu>
2021-12-29 14:44:54 -0500

    Fix type casing bug.

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Head from "next/head";
-import Header from "./Header/header";
+import Header from "./Header/Header";
 
 export const siteTitle = "Next.js Sample Website";
 
