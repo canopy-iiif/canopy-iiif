19631534065c4227aad0eff8b61b68f4cc084ab3
Mat Jordan <mat@northwestern.edu>
2021-12-30 11:49:15 -0500

    Reset config to defaults.

--- can.config.js
+++ can.config.js
@@ -2,16 +2,13 @@
  * Configuration built by `npm run build`
  */
 const config = {
-  collection:
-    "https://digital.lib.utk.edu/assemble/collection/collections/heilman",
+  collection: "https://can-the.vercel.app/iiif/collection/north-american.json",
+  title: "The North Americans",
   hero: [
-    "https://digital.lib.utk.edu/assemble/manifest/heilman/1187",
-    "https://digital.lib.utk.edu/assemble/manifest/heilman/572",
-    "https://digital.lib.utk.edu/assemble/manifest/heilman/181",
-    "https://digital.lib.utk.edu/assemble/manifest/heilman/270",
+    "https://can-the.vercel.app/iiif/manifest/apache/04-apache-camp.json",
+    "https://can-the.vercel.app/iiif/manifest/nez-perce/08-map-territory.json",
   ],
-  metadata: ["Coverage", "Date", "Topic"],
-  title: "Botanical Photography of Alan S. Heilman",
+  metadata: ["Date", "Subject"],
 };
 
 /**
@@ -32,16 +29,17 @@ const dev = {
  */
 const devAlt = {
   collection:
-    "https://digital.lib.utk.edu/assemble/collection/collections/heilman",
+    "https://digital.lib.utk.edu/assemble/collection/collections/tenncities",
   hero: [
-    "https://digital.lib.utk.edu/assemble/manifest/heilman/1187",
-    "https://digital.lib.utk.edu/assemble/manifest/heilman/572",
-    "https://digital.lib.utk.edu/assemble/manifest/heilman/181",
-    "https://digital.lib.utk.edu/assemble/manifest/heilman/270",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/365",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/289",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/419",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/435",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/443",
   ],
   metadata: ["Coverage", "Date", "Topic"],
-  title: "Botanical Photography of Alan S. Heilman",
+  title: "Images of Eastern Tennessee",
 };
 
 exports.config = config;
-exports.dev = devAlt;
+exports.dev = dev;
