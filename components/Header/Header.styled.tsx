05a2f9b2d7a1704f5459365f160d7edd080ea27b
Mat Jordan <mat@northwestern.edu>
2022-01-07 12:04:19 -0500

    Refine nav and header styling.

--- components/Header/Header.styled.tsx
+++ components/Header/Header.styled.tsx
@@ -3,16 +3,16 @@ import { styled } from "../../stiches.config";
 const Content = styled("div", {});
 
 const Title = styled("span", {
-  fontWeight: "650",
+  fontWeight: "300",
 });
 
 const Wrapper = styled("header", {
   position: "fixed",
-  zIndex: "1",
+  zIndex: "2",
   top: "0",
   width: "calc(100% - 4rem)",
   backgroundColor: "$indigo12",
-  background: "linear-gradient(90deg, $indigo12 0%, $mauve12 100%)",
+  background: "linear-gradient(90deg, $indigo12 0%, $indigo11 100%)",
   padding: "1rem 2rem",
   display: "flex",
   flexDirection: "row",
@@ -30,7 +30,7 @@ const Wrapper = styled("header", {
 
   nav: {
     a: {
-      fontWeight: "350",
+      fontWeight: "300",
       display: "inline-flex",
       fontSize: "1rem",
       marginRight: "1.25rem",
--- components/Nav/Nav.styled.ts
+++ components/Nav/Nav.styled.ts
@@ -1,25 +1,30 @@
 import { styled } from "../../stiches.config";
 const Wrapper = styled("nav", {
-  padding: "2rem 2rem 1rem",
-  margin: "auto",
+  padding: "1rem 2rem",
+  margin: "auto auto 1rem",
   position: "relative",
   zIndex: "1",
+  backgroundColor: "$mauve2",
+  boxShadow: "3px 3px 8px #0002",
 
   a: {
-    fontWeight: "350",
-    fontSize: "1.5rem",
+    fontSize: "1.25rem",
     display: "inline-flex",
-    marginRight: "1rem",
-    color: "$mauve11",
+    marginRight: "0.5rem",
+    color: "$mauve12",
     textDecoration: "none",
+    padding: "0.5rem 1rem",
+    borderRadius: "2rem",
+    fontWeight: "300",
 
     [`&:hover`]: {
       color: "$indigo11",
     },
 
     [`&.active`]: {
-      fontWeight: "650",
-      color: "$mauve12",
+      color: "$mauve1",
+      backgroundColor: "$indigo12",
+      background: "linear-gradient(90deg, $indigo11 0%, $indigo10 100%)",
     },
   },
 });
