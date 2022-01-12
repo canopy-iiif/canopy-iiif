518822d07ee49ea4336c3eb9ca079ba4923fec1a
Mat Jordan <mat@northwestern.edu>
2022-01-11 19:34:01 -0500

    Add RFTA example.

--- can.config.js
+++ can.config.js
@@ -2,36 +2,22 @@
  * Configuration built by `npm run build`
  */
 const config = {
-  collection: "https://can-the.vercel.app/iiif/collection/north-american.json",
-  title: "The North Americans",
-  hero: [
-    "https://can-the.vercel.app/iiif/manifest/apache/04-apache-camp.json",
-    "https://can-the.vercel.app/iiif/manifest/nez-perce/08-map-territory.json",
-  ],
-  metadata: ["Subject", "Date", "Dimensions"],
+  collection:
+    "https://digital.lib.utk.edu/static/iiif/collections/rfta_completed.json",
+  title: "Rising from the Ashes",
+  hero: ["https://digital.lib.utk.edu/assemble/manifest/rfta/6"],
+  metadata: ["Subject", "Place", "Date", "Format", "Language", "Interviewer"],
 };
 
 /**
  * Configuration built by `npm run dev`
  */
 const dev = {
-  collection: "http://localhost:5001/iiif-dev/collection/north-american.json",
-  title: "The North Americans",
-  hero: [
-    "http://localhost:5001/iiif-dev/manifest/apache/04-apache-camp.json",
-    "http://localhost:5001/iiif-dev/manifest/nez-perce/08-map-territory.json",
-    "http://localhost:5001/iiif-dev/manifest/tsawatenok/05-tsawatenok-house-front.json",
-  ],
-  metadata: ["Subject", "Date", "Dimensions"],
-};
-
-const devAlt = {
-  collection: "https://iiif.bodleian.ox.ac.uk/iiif/collection/canonici",
-  title: "Canonici",
-  hero: [
-    "https://iiif.bodleian.ox.ac.uk/iiif/manifest/d6f0ea37-6882-42a5-a923-9fb1e034ae29.json",
-  ],
-  metadata: ["Materials"],
+  collection:
+    "https://digital.lib.utk.edu/static/iiif/collections/rfta_completed.json",
+  title: "Rising from the Ashes",
+  hero: ["https://digital.lib.utk.edu/assemble/manifest/rfta/6"],
+  metadata: ["Subject", "Place", "Date", "Format", "Language", "Interviewer"],
 };
 
 exports.config = config;
