f745b08fef85c60e6fc6e97c698ad096cbbc2af5
Mat Jordan <mat@northwestern.edu>
2022-01-11 20:32:03 -0500

    Experiment with basic search.

---
+++ pages/api/search.ts
@@ -0,0 +1,32 @@
+import { getCollectionData } from "../../services/iiif";
+
+export default async (req, res) => {
+  getCollectionData().then((tree) => {
+    Promise.all(tree)
+      .then((values) => {
+        let items = [];
+        values.forEach((results) => {
+          if (results)
+            results.items.forEach((element) => {
+              items.push(element);
+            });
+        });
+        let manifests = items.filter((item) => item.type === "Manifest");
+
+        if (req.query.q)
+          manifests = req.query.q
+            ? manifests.filter((manifest) =>
+                manifest.label[0].toLowerCase().includes(req.query.q)
+              )
+            : [];
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({ manifests }));
+      })
+      .catch((error) => {
+        res.json(error);
+        res.status(405).end();
+      });
+  });
+};
