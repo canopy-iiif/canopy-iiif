6c9e814ea588a66e5cea8e0b3f9cabbafae34340
Mark Baggett <mbagget1@utk.edu>
2022-04-08 09:45:08 -0400

    Switch config and assume service is an array.

--- can.config.js
+++ can.config.js
@@ -2,28 +2,26 @@
  * Configuration built by `npm run build`
  */
 const config = {
-  collection: "https://can-the.vercel.app/iiif/collection/north-american.json",
-  title: "The North Americans",
+  collection: "https://digital.lib.utk.edu/assemble/collection/collections/bcpl",
+  title: "RFTA Artists",
   hero: [
-    "https://can-the.vercel.app/iiif/manifest/apache/04-apache-camp.json",
-    "https://can-the.vercel.app/iiif/manifest/nez-perce/08-map-territory.json",
+    "https://digital.lib.utk.edu/assemble/manifest/bcpl/185",
   ],
-  metadata: ["Subject", "Date", "Dimensions"],
+  metadata: ["Subject"],
 };
 
 /**
  * Configuration built by `npm run dev`
  */
 const dev = {
-  collection: "http://localhost:5001/iiif-dev/collection/north-american.json",
-  title: "The North Americans",
+  collection: "https://digital.lib.utk.edu/assemble/collection/collections/bcpl",
+  title: "RFTA Artists",
   hero: [
-    "http://localhost:5001/iiif-dev/manifest/apache/04-apache-camp.json",
-    "http://localhost:5001/iiif-dev/manifest/nez-perce/08-map-territory.json",
-    "http://localhost:5001/iiif-dev/manifest/tsawatenok/05-tsawatenok-house-front.json",
+    "https://digital.lib.utk.edu/assemble/manifest/bcpl/185",
   ],
-  metadata: ["Subject", "Date", "Dimensions"],
+  metadata: ["Subject"],
 };
+
 
 const devAlt = {
   collection: "https://iiif.bodleian.ox.ac.uk/iiif/collection/canonici",
