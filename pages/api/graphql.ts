731bfc07c4e99c8e294f0bd96011667901452cef
Mark Baggett <mbagget1@utk.edu>
2022-05-27 20:23:37 -0400

    Initialize query to get children of metadata collections.

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -13,6 +13,7 @@ const typeDefs = gql`
     collectionItems: [CollectionItem]
     manifests(limit: Int, offset: Int, id: [String]): [Manifest]
     metadata(id: String, label: String): [Metadata]
+    metadataItems(id: String, label: String, valueOfMetadata: String): [Metadata]
     allManifests: [Manifest]
     getManifest(slug: ID): Manifest
   }
@@ -136,6 +137,56 @@ const resolvers = {
                         thumbnail: manifest.thumbnail[0].id,
                       };
                       data.push(result);
+                    });
+                  }
+                });
+              });
+            return data;
+          });
+        });
+      });
+    },
+    metadataItems: async (_, { id, label, valueOfMetadata }, context) => {
+      let filterByLabels = process.env.metadata;
+      if (label) filterByLabels = [label as string];
+
+      return getCollectionData().then((tree) => {
+        return Promise.all(tree).then((values) => {
+          let items = [];
+          if (id) {
+            items.push({ id, type: "Manifest" });
+          } else {
+            values.forEach((results) => {
+              if (results)
+                results.items.forEach((element) => {
+                  items.push(element);
+                });
+            });
+          }
+
+          const responses = getBulkManifests(items, 10);
+
+          return responses.then((manifests) => {
+            let data = [];
+            manifests
+              .filter((manifest) => {
+                if (manifest) return manifest;
+              })
+              .map((manifest) => {
+                manifest.metadata.forEach((metadata) => {
+                  const metadataLabel = getValues(metadata.label)[0];
+                  const metadataValues = getValues(metadata.value);
+                  if (filterByLabels.includes(metadataLabel)) {
+                    metadataValues.forEach((value) => {
+                      if(value === valueOfMetadata){
+                        const result = {
+                          manifestId: manifest.id,
+                          label: metadataLabel,
+                          value,
+                          thumbnail: manifest.thumbnail[0].id,
+                        };
+                        data.push(result);
+                      }
                     });
                   }
                 });
