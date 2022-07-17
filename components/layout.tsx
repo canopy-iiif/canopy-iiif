56187d8a99058e2c929cc84f750b41044ceb44e8
Mark Baggett <mbagget1@utk.edu>
2022-07-17 11:19:21 -0500

    Remove initial metatags and fix favicon.

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,19 +8,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <Header />
       <main>{children}</main>
