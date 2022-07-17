42be29eaccebd9b824014db680334bb2d3f1573d
Mark Baggett <mbagget1@utk.edu>
2022-07-17 11:20:25 -0500

    Remove unused variable.

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import Head from "next/head";
 import Header from "@/components/Header/Header";
 
-export const siteTitle = "Next.js Sample Website";
-
 export default function Layout({ children }) {
   return (
     <>
