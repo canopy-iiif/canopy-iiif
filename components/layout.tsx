aa4c5a07c0e99628e26797afaf70b0a14c0fe93c
Mark Baggett <mbagget1@utk.edu>
2022-07-21 10:00:24 -0400

    Update components/layout.tsx
    Co-authored-by: Mat Jordan <mat@northwestern.edu>

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import Header from "@/components/Header/Header";
-import BaseTag from "./Metatag/BaseTag";
+import BaseTag from "@/components/Metatag/BaseTag";
 import Analytics from "./Analytics/Analytics";
 
 export default function Layout({ children }) {
