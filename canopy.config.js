1f145b584d5b211df5a6be067ca4dc36974418f3
Mat Jordan <mat@northwestern.edu>
2022-10-25 20:21:43 -0400

    Point to Github raw source.

--- canopy.config.js
+++ canopy.config.js
@@ -3,10 +3,10 @@
  */
 exports.prod = {
   collection:
-    "https://canopy-iiif.vercel.app/fixtures/iiif/collection/nez-perce.json",
+    "https://raw.githubusercontent.com/mathewjordan/canopy-iiif/main/public/fixtures/iiif/collection/nez-perce.json",
   title: "Nez Percé (Nimíipuu)",
   hero: [
-    "https://canopy-iiif.vercel.app/fixtures/iiif/manifest/nez-perce/02-lawyer.json",
+    "https://raw.githubusercontent.com/mathewjordan/canopy-iiif/main/public/fixtures/iiif/manifest/nez-perce/02-lawyer.json",
   ],
   metadata: ["Date", "Subject"],
 };
