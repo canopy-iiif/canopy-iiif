2846bbb4f23b1d836d8b9081ff06681f76e9711d
Mat Jordan <mat@northwestern.edu>
2022-11-11 04:59:31 -0500

    Tenncities.

--- canopy.config.js
+++ canopy.config.js
@@ -2,30 +2,28 @@
  * Configuration built by `npm run build`
  */
 exports.prod = {
-  label: { none: ["Canopy IIIF"] },
   collection:
-    "https://api.dc.library.northwestern.edu/api/v2/search?query=%22Edward%20S.%20Curtis%27s%20The%20North%20American%20Indian%20--%20Volume%208.%20The%20Nez%20Perces.%20Wallawalla.%20Umatilla.%20Cayuse.%20The%20Chinookan%20tribes%22&as=iiif&size=500",
+    "https://digital.lib.utk.edu/assemble/collection/collections/tenncities",
   featured: [
-    "https://api.dc.library.northwestern.edu/api/v2/works/588a6527-88b6-4b70-ae6c-f42aceea9efb?as=iiif",
-    "https://api.dc.library.northwestern.edu/api/v2/works/14a6aa15-9e12-47a7-9617-91f430d4f47b?as=iiif",
-    "https://api.dc.library.northwestern.edu/api/v2/works/15871197-1616-4c37-9257-b0ad0dc5ab30?as=iiif",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/467",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/354",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/345",
   ],
-  metadata: ["Date", "Dimensions", "Genre", "Subject"],
+  metadata: ["Subject", "Date", "Place"],
 };
 
 /**
  * Configuration built by `npm run dev`
  */
 exports.dev = {
-  label: { none: ["Canopy IIIF"] },
   collection:
-    "https://api.dc.library.northwestern.edu/api/v2/search?query=%22Edward%20S.%20Curtis%27s%20The%20North%20American%20Indian%20--%20Volume%208.%20The%20Nez%20Perces.%20Wallawalla.%20Umatilla.%20Cayuse.%20The%20Chinookan%20tribes%22&as=iiif&size=500",
+    "https://digital.lib.utk.edu/assemble/collection/collections/tenncities",
   featured: [
-    "https://api.dc.library.northwestern.edu/api/v2/works/588a6527-88b6-4b70-ae6c-f42aceea9efb?as=iiif",
-    "https://api.dc.library.northwestern.edu/api/v2/works/14a6aa15-9e12-47a7-9617-91f430d4f47b?as=iiif",
-    "https://api.dc.library.northwestern.edu/api/v2/works/15871197-1616-4c37-9257-b0ad0dc5ab30?as=iiif",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/467",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/354",
+    "https://digital.lib.utk.edu/assemble/manifest/tenncities/345",
   ],
-  metadata: ["Date", "Dimensions", "Genre", "Subject"],
+  metadata: ["Subject", "Date", "Place"],
 };
 
 exports.options = {
