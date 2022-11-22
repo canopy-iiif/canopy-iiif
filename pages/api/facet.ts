55a4791a2a2d56058a231a1aa416edfb4e6fd842
Mat Jordan <mat@northwestern.edu>
2022-11-22 13:29:44 -0500

    Reintroduce Bloom.

---
+++ pages/api/facet.ts
@@ -0,0 +1,29 @@
+import FACETS from "@/.canopy/facets.json";
+import absoluteUrl from "next-absolute-url";
+
+export default function handler(request, response) {
+  const { origin } = absoluteUrl(request);
+  const { query, url } = request;
+  const { label, value } = query;
+
+  const baseUrl = origin + url;
+
+  const facet = FACETS.find((entry) => entry.slug === label);
+  const items = facet.values.map((value) => {
+    return {
+      id: `${baseUrl}&value=${value.value}`,
+      type: "Collection",
+      label: { none: [value.value] },
+      summary: { none: [`${value.doc_count} Items`] },
+    };
+  });
+
+  response.status(200).json({
+    "@context": "https://iiif.io/api/presentation/3/context.json",
+    id: baseUrl,
+    type: "Collection",
+    label: { none: [facet.label] },
+    summary: { none: [`${facet.values.length} Terms`] },
+    items: items,
+  });
+}
