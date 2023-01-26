f1492ca3bcd73d3f19728579eff0713b4b0e6fa4
Mat Jordan <mat@northwestern.edu>
2023-01-26 09:39:20 -0500

    Update primary demo example.

--- canopy.config.js
+++ canopy.config.js
@@ -3,11 +3,13 @@
  */
 exports.prod = {
   collection:
-    "https://raw.githubusercontent.com/mathewjordan/combine/main/output/ww2.json",
+    "https://api.dc.library.northwestern.edu/api/v2/search?query=%22Edward%20S.%20Curtis%27s%20The%20North%20American%20Indian%20--%20Volume%208.%20The%20Nez%20Perces.%20Wallawalla.%20Umatilla.%20Cayuse.%20The%20Chinookan%20tribes%22&as=iiif&size=500",
   featured: [
-    "https://api.dc.library.northwestern.edu/api/v2/works/881e18e9-d40c-4ebb-8840-b3e2193e8e01?as=iiif",
+    "https://api.dc.library.northwestern.edu/api/v2/works/588a6527-88b6-4b70-ae6c-f42aceea9efb?as=iiif",
+    "https://api.dc.library.northwestern.edu/api/v2/works/14a6aa15-9e12-47a7-9617-91f430d4f47b?as=iiif",
+    "https://api.dc.library.northwestern.edu/api/v2/works/15871197-1616-4c37-9257-b0ad0dc5ab30?as=iiif",
   ],
-  metadata: ["Date", "Subject"],
+  metadata: ["Date", "Dimensions", "Genre", "Subject"],
 };
 
 /**
@@ -15,11 +17,13 @@ exports.prod = {
  */
 exports.dev = {
   collection:
-    "https://raw.githubusercontent.com/mathewjordan/combine/main/output/ww2.json",
+    "https://api.dc.library.northwestern.edu/api/v2/search?query=%22Edward%20S.%20Curtis%27s%20The%20North%20American%20Indian%20--%20Volume%208.%20The%20Nez%20Perces.%20Wallawalla.%20Umatilla.%20Cayuse.%20The%20Chinookan%20tribes%22&as=iiif&size=500",
   featured: [
-    "https://api.dc.library.northwestern.edu/api/v2/works/881e18e9-d40c-4ebb-8840-b3e2193e8e01?as=iiif",
+    "https://api.dc.library.northwestern.edu/api/v2/works/588a6527-88b6-4b70-ae6c-f42aceea9efb?as=iiif",
+    "https://api.dc.library.northwestern.edu/api/v2/works/14a6aa15-9e12-47a7-9617-91f430d4f47b?as=iiif",
+    "https://api.dc.library.northwestern.edu/api/v2/works/15871197-1616-4c37-9257-b0ad0dc5ab30?as=iiif",
   ],
-  metadata: ["Date", "Subject"],
+  metadata: ["Date", "Dimensions", "Genre", "Subject"],
 };
 
 exports.options = {
