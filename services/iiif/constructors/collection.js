f7061a115842366d57e3b9d049c0e8d074114fc5
Mark Baggett <mbagget1@utk.edu>
2023-02-01 18:55:42 -0500

    Clean up. :do_not_litter:

--- services/iiif/constructors/collection.js
+++ services/iiif/constructors/collection.js
@@ -7,7 +7,6 @@ const { getRepresentativeImage } = require("../image");
 async function createCollection(iiifResources, label = "") {
   const items = await getCollectionItems(iiifResources);
   const complete_items = await Promise.all(items.map(async ( resource) => await createItem(resource)))
-  console.log(complete_items)
   return {
     "@context": "https://iiif.io/api/presentation/3/context.json",
     id: `http://localhost/featured`,
@@ -20,7 +19,6 @@ async function createCollection(iiifResources, label = "") {
 async function createItem(resource) {
   const { slug } = MANIFESTS.find((item) => item.id === resource.id);
   const thumbnail =  await getRepresentativeImage(resource, 2000);
-  // console.log(thumbnail);
   return {
     id: resource.id,
     label: resource.label,
