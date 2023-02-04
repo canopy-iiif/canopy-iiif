28fad5a36ee95b240486b1a263aa4bad7afb882c
Mark Baggett <mbagget1@utk.edu>
2023-02-04 14:58:14 -0500

    Clean up. :do_not_litter:

--- components/Grid/Item.tsx
+++ components/Grid/Item.tsx
@@ -1,34 +1,17 @@
-import React, { useEffect, useState } from "react";
 import Card from "@/components/Card/Card";
-import { getJsonByURI } from "@/services/utils";
 import { GridItem as ItemStyled } from "@/components/Grid/Grid.styled";
 import { getLabel } from "@/hooks/getLabel";
-import { getPresentation3 } from "@/services/iiif/context";
 
 const GridItem = ({ data }) => {
-  const [item, setItem] = useState();
   const path = data.homepage[0].id;
 
-  useEffect(() => {
-    getJsonByURI(data.id).then((json) => setItem(getPresentation3(json)));
-  }, []);
-
-  let resource;
-
-  if (!item) return <></>;
-
-  /**
-   * @todo: handle this better
-   */
-  if (item.items) resource = item.items[0].items[0].items[0].body;
-
-  const aspectRatio = resource.width / resource.height;
+  const aspectRatio = data.thumbnail[0].width / data.thumbnail[0].height;
 
   return (
     <ItemStyled>
       <Card
         key={data.id}
-        label={getLabel(item?.label)}
+        label={getLabel(data?.label)}
         path={path}
         resource={data.thumbnail}
         aspectRatio={aspectRatio}
