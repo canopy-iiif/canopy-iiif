801120c6938d42960b0a8cb4ec3be04bfbb76dc8
Mat Jordan <mat@northwestern.edu>
2023-02-05 15:53:52 -0500

    Style metadata list.

--- pages/metadata.tsx
+++ pages/metadata.tsx
@@ -4,13 +4,43 @@ import Layout from "@/components/layout";
 import Container from "../components/Shared/Container";
 import Link from "next/link";
 import Heading from "@/components/Shared/Heading/Heading";
+import { styled } from "@/stitches";
+import {
+  ArrowRightIcon,
+  BookmarkFilledIcon,
+  DoubleArrowRightIcon,
+  MinusIcon,
+  TriangleRightIcon,
+} from "@radix-ui/react-icons";
+
+const ListItem = styled("li", {
+  listStyle: "none",
+  padding: "0",
+  margin: "0 0 $gr1",
+
+  svg: {
+    content: "",
+    display: "block",
+    position: "absolute",
+    borderRadius: "50%",
+    marginLeft: "-$gr4",
+    marginTop: "5px",
+    color: "$indigo11",
+  },
+
+  em: {
+    fontSize: "$gr3",
+    fontStyle: "normal",
+  },
+});
 
 export default function Metadata() {
   const MetadataItem = ({ value, slug, doc_count, path }) => {
     return (
-      <li>
-        <Link href={`${path}${slug}`}>{value}</Link> <em>{doc_count}</em>
-      </li>
+      <ListItem>
+        <MinusIcon />
+        <Link href={`${path}${slug}`}>{value}</Link> <em>({doc_count})</em>
+      </ListItem>
     );
   };
 
