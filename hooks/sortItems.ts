fbd222724a096db1f58a525d1d006228439ed0c1
Mark Baggett <mbagget1@utk.edu>
2023-02-05 11:50:18 -0500

    Add reverse and init lexical sort.

--- hooks/sortItems.ts
+++ hooks/sortItems.ts
@@ -7,6 +7,12 @@ export const sortItems = (
       .sort((a, b) => a.sort - b.sort)
       .map(({ value }) => value);
   }
+  else if (sortType === "reverse") {
+    return items.reverse();
+  }
+  else if (sortType === "lexical") {
+    return items.sort((a,b) => (a.label.none[0] > b.label.none[0]) ? 1 : ((b.label.none[0] > a.label.none[0]) ? -1 : 0))
+  }
   else {
     return items
   }
