c44a07f1f6c2302f7c7a1f3e87a6d2e0b48d3baa
Mark Baggett <mbagget1@utk.edu>
2023-02-05 16:41:23 -0500

    Switch to switch.

--- hooks/sortItems.ts
+++ hooks/sortItems.ts
@@ -2,31 +2,28 @@ export const sortItems = (
   items: Array,
   sortType: string = "none"
 ) => {
-  if (sortType === "random") {
-    return items.map(value => ({ value, sort: Math.random() }))
-      .sort((a, b) => a.sort - b.sort)
-      .map(({ value }) => value);
-  }
-  else if (sortType === "reverse") {
-    return items.reverse();
-  }
-  else if (sortType === "lexical") {
-    return items.sort(
-      (a,b) =>
-        (a.label[Object.keys(a.label)[0]][0] > b.label[Object.keys(b.label)[0]][0]) ? 1 : (
-          (b.label[Object.keys(b.label)[0]][0] > a.label[Object.keys(a.label)[0]][0]) ? -1 : 0
-        )
-    )
-  }
-  else if (sortType === "reverse-lexical") {
-    return items.sort(
-      (a,b) =>
-        (b.label[Object.keys(b.label)[0]][0] > a.label[Object.keys(a.label)[0]][0]) ? 1 : (
-          (a.label[Object.keys(a.label)[0]][0] > b.label[Object.keys(b.label)[0]][0]) ? -1 : 0
-        )
-    )
-  }
-  else {
-    return items
+  switch (sortType) {
+    case "random":
+      return items.map(value => ({ value, sort: Math.random() }))
+        .sort((a, b) => a.sort - b.sort)
+        .map(({ value }) => value);
+    case "lexical":
+      return items.sort(
+        (a,b) =>
+          (a.label[Object.keys(a.label)[0]][0] > b.label[Object.keys(b.label)[0]][0]) ? 1 : (
+            (b.label[Object.keys(b.label)[0]][0] > a.label[Object.keys(a.label)[0]][0]) ? -1 : 0
+          )
+      )
+    case "reverse-lexical":
+      return items.sort(
+        (a,b) =>
+          (b.label[Object.keys(b.label)[0]][0] > a.label[Object.keys(a.label)[0]][0]) ? 1 : (
+            (a.label[Object.keys(a.label)[0]][0] > b.label[Object.keys(b.label)[0]][0]) ? -1 : 0
+          )
+      )
+    case "reverse":
+      return items.reverse();
+    default:
+      return items
   }
 };
