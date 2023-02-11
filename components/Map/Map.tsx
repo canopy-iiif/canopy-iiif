fee4c294fea9e33e4cd1063f572950fb62f32faf
Mark Baggett <mbagget1@utk.edu>
2023-02-11 18:31:10 -0500

    Switch bounding.

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -18,10 +18,10 @@ const Map =({manifests, bounds}) => {
   if (bounds.length < 1){
     bounds = [[-0.09, 51.505]]
   }
-  console.log(manifests);
+  console.log(bounds);
   return (
     <MapStyled>
-      <MapContainer className={"map-container"} bounds={bounds} center={bounds[0].reverse()} zoom={8} scrollWheelZoom={false}>
+      <MapContainer className={"map-container"} bounds={bounds} center={bounds[0]} zoom={8} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
