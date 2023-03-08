cbd613e457bd04fa6eb79a77d4084e218cc04e8b
Mat Jordan <mat@northwestern.edu>
2023-03-08 09:59:38 -0500

    Add scrollTo behavior.

--- components/Nav/Items.tsx
+++ components/Nav/Items.tsx
@@ -1,37 +1,21 @@
 import { Items } from "@/components/Nav/Nav.styled";
-import Link from "next/link";
+import NavItemsLink from "@/components/Nav/ItemsLink";
 import React from "react";
-import { useRouter } from "next/router";
-import Nav from "./Nav";
 
 interface NavItemsProps {
   items: any;
   orientation: "horizontal" | "vertical";
   subNavigation?: any;
 }
-
 const NavItems: React.FC<NavItemsProps> = ({
   items,
   orientation,
   subNavigation,
 }) => {
-  const router = useRouter();
-
   return (
     <Items orientation={orientation}>
       {items.map((item: any) => (
-        <>
-          <Link
-            href={item.path}
-            key={item.path}
-            className={router.pathname == item.path ? "active" : ""}
-          >
-            {item.text}
-          </Link>
-          {subNavigation && item.path === router.pathname && (
-            <Nav items={subNavigation} orientation={"vertical"} />
-          )}
-        </>
+        <NavItemsLink {...item} key={item.path} subNavigation={subNavigation} />
       ))}
     </Items>
   );
