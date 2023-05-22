5ccfbe4731c1ba175f0c6a46639980bc8b40163e
mat <mat@northwestern.edu>
2023-05-21 23:17:28 -0400

    Breakout FlexSearch configuration to options.json. (#123)
    * Breakout FlexSearch configuration to options.json.
    
    * Update docs json.
    
    * Remove empty import.

--- services/search/documents.ts
+++ services/search/documents.ts
@@ -1,38 +1,14 @@
+// @ts-nocheck
+
 import INDEX from "@/.canopy/index.json";
 import { Document } from "flexsearch";
 
 const getDocuments = (q: string) => {
-  //@ts-ignore
-  const index = new Document({
-    charset: "latin:extra",
-    optimize: true,
-    tokenize: "strict",
-    bidirectional: false,
-    document: {
-      index: [
-        {
-          field: "label",
-          tokenize: "extra",
-          resolution: 9,
-          depth: 3,
-          bidirectional: true,
-        },
-        {
-          field: "metadata",
-          resolution: 2,
-        },
-        {
-          field: "summary",
-          resolution: 1,
-        },
-      ],
-    },
-  });
+  const { flexSearch } = process.env?.CANOPY_CONFIG?.search;
 
-  //@ts-ignore
+  const index = new Document(flexSearch);
   INDEX.forEach((doc) => index.add(doc));
 
-  //@ts-ignore
   const results = index.search(q).reduce((acc, curr) => {
     return [...new Set([...acc, ...curr.result])];
   }, []);
