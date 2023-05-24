95e419a1be050b19b79e7f844a68131a44b51ec3
Mark Baggett <mbagget1@utk.edu>
2023-05-24 08:00:04 -0400

    Add canonical urls.

--- components/Metatag/Metatag.tsx
+++ components/Metatag/Metatag.tsx
@@ -2,6 +2,8 @@ import { NextSeo } from 'next-seo';
 import { getValues } from '@/hooks/getValues';
 import { InternationalString } from "@iiif/presentation-3";
 import { getResourceImage } from "@/hooks/getResourceImage";
+import React, {useEffect, useState} from "react";
+import { useRouter } from "next/router";
 
 interface MetatagProps {
   label: InternationalString;
@@ -10,9 +12,17 @@ interface MetatagProps {
 }
 
 const Metatag: React.FunctionComponent<MetatagProps> = ({ label, summary, thumbnail }) => {
+  const [baseUrl, setBaseUrl] = useState("");
+  const router = useRouter();
   const labelValue = getValues(label);
   const summaryValue = getValues(summary);
   const thumbnailValue = getResourceImage(thumbnail);
+  useEffect(() => {
+    const { host, protocol } = window.location;
+    const root = `${protocol}//${host}`;
+    setBaseUrl(root);
+  }, []);
+  const canonicalUrl = ( baseUrl + (router.asPath === "/" ? "": router.asPath)).split("?")[0];
 
   return (
     <NextSeo
@@ -22,6 +32,7 @@ const Metatag: React.FunctionComponent<MetatagProps> = ({ label, summary, thumbn
         {
           title: labelValue,
           description: summaryValue,
+          url: canonicalUrl,
           images: [
             {
               url: thumbnailValue,
