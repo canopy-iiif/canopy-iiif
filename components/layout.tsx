ab361868f47227014a0acde549fb77d11bfbb057
Mark Baggett <mbagget1@utk.edu>
2023-05-26 13:28:43 -0400

    Remove basetag.

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,12 +2,10 @@ import React, { ReactNode } from "react";
 import Header from "@/components/Header/Header";
 import Main from "./Shared/Main";
 import Footer from "./Footer/Footer";
-import BaseTag from "@/components/Metatag/Basetag";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <BaseTag />
       <Header />
       <Main>{children}</Main>
       <Footer />
