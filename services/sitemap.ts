f82a899f0e9270806e4cc5ebac63b3d9c43884c7
Mark Baggett <mbagget1@utk.edu>
2023-05-29 13:10:38 -0400

    Fix slugs.

--- services/sitemap.ts
+++ services/sitemap.ts
@@ -5,19 +5,11 @@ const generateSitemap = async () => {
   const manifests = canopyManifests();
   const { url } = process.env
     ?.CANOPY_CONFIG as unknown as CanopyEnvironment;
-  const slugs = manifests.map((manifest) => `${url}/${manifest.slug}`);
-  console.log(slugs);
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
-     ${slugs
-    .map(({ slug }) => {
-      return `
-       <url>
-           <loc>${`${url}/${slug}`}</loc>
-       </url>
-     `;
-    })
+     ${manifests.map(
+       (manifest) => `<url>${url}/${manifest.slug}</url>`
+  )
     .join('')}
    </urlset>
  `;
