f6ef809a9ce3e3a86724a2ff6a01a9bf0a19f5bf
Mark Baggett <mbagget1@utk.edu>
2023-05-29 17:27:23 -0400

    Add navItems.

--- services/sitemap.ts
+++ services/sitemap.ts
@@ -5,11 +5,18 @@ const generateSitemap = async () => {
   const manifests = canopyManifests();
   const { url } = process.env
     ?.CANOPY_CONFIG as unknown as CanopyEnvironment;
+  const navItems = process.env.CANOPY_CONFIG.navigation;
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>${url}</url>
+     ${navItems.primary.map(
+       (navItem) => `<url>${url}${navItem.path}</url>`
+     ).join('')}
+     ${navItems.about.map(
+       (navItem) => `<url>${url}${navItem.path}</url>`
+     ).join('')}
      ${manifests.map(
-       (manifest) => `<url>${url}/${manifest.slug}</url>`
-  )
+       (manifest) => `<url>${url}/${manifest.slug}</url>`)
     .join('')}
    </urlset>
  `;
