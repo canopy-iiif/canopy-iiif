a92538e8ff88ae01e91df882c0834bd430c63231
Mark Baggett <mbagget1@utk.edu>
2023-05-29 18:40:22 -0400

    Add full response.

--- services/sitemap.ts
+++ services/sitemap.ts
@@ -1,23 +1,39 @@
 import {canopyManifests} from "@/services/constants/canopy";
 import { CanopyEnvironment } from "@/types/canopy";
 
+function buildUrls(urls, domain, location, divider='') {
+  const today = new Date();
+  const currentDate = today.toISOString().split('T')[0];
+  return `
+    ${urls.map(
+      (url) => `<url>
+        <loc>${domain}${divider}${url[location]}</loc>
+        <lastmod>${currentDate}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+      </url>`
+    ).join('')}
+  `
+}
+
 const generateSitemap = async () => {
   const manifests = canopyManifests();
   const { url } = process.env
     ?.CANOPY_CONFIG as unknown as CanopyEnvironment;
   const navItems = process.env.CANOPY_CONFIG.navigation;
+  const today = new Date();
+  const currentDate = today.toISOString().split('T')[0];
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <url>${url}</url>
-     ${navItems.primary.map(
-       (navItem) => `<url>${url}${navItem.path}</url>`
-     ).join('')}
-     ${navItems.about.map(
-       (navItem) => `<url>${url}${navItem.path}</url>`
-     ).join('')}
-     ${manifests.map(
-       (manifest) => `<url>${url}/${manifest.slug}</url>`)
-    .join('')}
+     <url>
+        <loc>${url}</loc>
+        <lastmod>${currentDate}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.8</priority>
+     </url>
+     ${buildUrls(navItems.primary, url, "path")}
+     ${buildUrls(navItems.about, url, "path")}
+     ${buildUrls(manifests, url, "slug", '/')}
    </urlset>
  `;
 }
