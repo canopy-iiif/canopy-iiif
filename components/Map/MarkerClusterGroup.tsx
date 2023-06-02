28af0dbe430f9c23cc438d51cb3cdba7baa9a81e
Mark Baggett <mbagget1@utk.edu>
2023-06-02 16:59:01 -0400

    Group markers in clusters.

---
+++ components/Map/MarkerClusterGroup.tsx
@@ -0,0 +1,25 @@
+import 'leaflet.markercluster';
+import { createPathComponent } from '@react-leaflet/core';
+import L from 'leaflet';
+
+const MarkerClusterGroup = createPathComponent(({children: _c, ...props}, ctx) => {
+  const clusterProps: Record<string, any> = {};
+  const clusterEvents: Record < string, any >= {};
+
+  Object.entries(props).forEach(([propName, prop]) => propName.startsWith('on') ? (clusterEvents[propName] = prop)
+    : (clusterProps[propName] = prop));
+
+  const markerClusterGroup = L.markerClusterGroup(clusterProps);
+
+  Object.entries(clusterEvents).forEach(([eventAsProp, callback]) => {
+    const clusterEvent = `cluster${eventAsProp.substring(2).toLowerCase()}`;
+    markerClusterGroup.on(clusterEvent, callback);
+  });
+
+  return {
+    instance: markerClusterGroup,
+    context: {...ctx, layerContainer: markerClusterGroup},
+  };
+});
+
+export default MarkerClusterGroup;
