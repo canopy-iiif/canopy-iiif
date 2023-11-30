8185274f43dce15ea6ec466e5e206c92ecbcf0fd
mat <mat@northwestern.edu>
2023-11-30 10:24:17 -0500

    Update about section reference new docs. (#169)

--- src/components/Shared/Button/Button.tsx
+++ src/components/Shared/Button/Button.tsx
@@ -1,20 +1,29 @@
-import Link from "next/link";
 import React, { ReactNode } from "react";
+
 import { ButtonStyled } from "./Button.styled";
+import Link from "next/link";
+
 Link;
 
 interface ButtonProps {
   buttonType?: "primary" | "secondary" | "transparent";
   children: ReactNode | ReactNode[];
   href?: string;
+  target?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ buttonType, children, href }) => {
+const Button: React.FC<ButtonProps> = ({
+  buttonType,
+  children,
+  href,
+  target,
+}) => {
   return (
     <ButtonStyled
       href={href}
       buttonType={buttonType}
       as={href ? Link : "button"}
+      target={target}
     >
       {children}
     </ButtonStyled>
