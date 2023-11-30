6fc2e916c7ec0d59ab15d1a48659a6f11a8dc00c
markpbaggett <mbagget1@utk.edu>
2023-11-30 13:58:12 +0000

    Deploying to gh-pages from @ canopy-iiif/canopy-iiif@2051e6d560b47e5bb7c526a434f5add44f888174 🚀

--- _next/static/DHp821cE9EmrFJB9l5o1U/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002Fabout","\u002Fabout\u002F[slug]","\u002Fmetadata","\u002Fworks\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
