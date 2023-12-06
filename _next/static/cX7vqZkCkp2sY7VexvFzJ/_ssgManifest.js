42953722bcb86426ed3b23b6889ae6c0f786dfcb
mathewjordan <mat@northwestern.edu>
2023-12-06 03:01:34 +0000

    Deploying to gh-pages from @ canopy-iiif/canopy-iiif@d547bb83086305db55c550386efeb3a4b0a3eba2 🚀

---
+++ _next/static/cX7vqZkCkp2sY7VexvFzJ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002F404","\u002Fabout","\u002Fabout\u002F[slug]","\u002Fmap","\u002Fmetadata","\u002Fsearch","\u002Fworks\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
