f90cfec6c45218865176f9a65082e0eda1f2fbfa
mat <mat@northwestern.edu>
2024-10-23 16:26:35 -0400

    Simplify height of Clover viewer, remove relative container.

--- src/components/Work/Viewer.tsx
+++ src/components/Work/Viewer.tsx
@@ -1,12 +1,9 @@
-import { Box } from "@radix-ui/themes";
 import { CloverViewerProps } from "@samvera/clover-iiif";
 import Viewer from "@src/components/Viewer/Viewer";
 
 const WorkViewer = (props: CloverViewerProps) => {
   return (
-    <Box height="50vh" position="relative">
-      <Viewer options={{ canvasHeight: "100%" }} {...props} />
-    </Box>
+    <Viewer options={{ canvasHeight: "50vh" }} {...props} />
   );
 };
 
