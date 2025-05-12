a5dbbf44b8d87d4b4d0e528a79d91927602057e6
Mark Baggett <markpbaggett@gmail.com>
2025-05-12 12:30:38 -0500

    Fix Issue-205.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,17 +8,20 @@ import Document, {
 
 import { GoogleFontImport } from "@styles/theme/fonts";
 import React from "react";
+import {CanopyEnvironment} from "@customTypes/canopy.ts";
 
 class CanopyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
-
     return initialProps;
   }
 
   render() {
+    const config = process.env.CANOPY_CONFIG as unknown as CanopyEnvironment;
+    const lang = config.locales[0].lang || 'en';
+
     return (
-      <Html>
+      <Html lang={lang}>
         <Head>
           <GoogleFontImport />
         </Head>
